411584d5009a9ef3279d242e8560a0da991c9870
Felipe <1900796M@escolas.anchieta.br>
2025-08-03 16:54:15 -0300

    AJU 4.1

--- To_xampp_server/php_functs/redirect_filter.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-if(isset($_GET['category_button'])){
-    $button = $_GET['category_button'];
-    header("Location: ../filter.php?type=".$_GET['type']."&category_button=$button&$button=on");
-    exit();
-}
-?>
